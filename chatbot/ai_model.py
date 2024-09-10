1bf5e98737a1e52248dd145f90dcbfc8d43d47f7
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-09-10 21:32:15 +0200

    ...

--- chatbot/ai_model.py
+++ chatbot/ai_model.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from transformers import pipeline
 
 # Initialize the Hugging Face model
@@ -10,3 +11,33 @@ def generate_huggingface_response(user_input):
         return response[0]['generated_text'].strip()
     except Exception as e:
         return f"Error generating response: {str(e)}"
+=======
+import openai
+import os
+
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+def generate_huggingface_response(user_input):
+    pass
+
+
+def generate_openai_response(user_input):
+    pass
+
+
+def generate_response(user_input):
+    try:
+        # Create a chat completion
+        response = openai.ChatCompletion.create(
+            model='gpt-3.5-turbo',
+            messages=[
+                {"role": "user", "content": user_input}
+            ]
+        )
+        # Access the response content
+        return response.choices[0]['message']['content'].strip()
+    except Exception as e:
+        return f"An error occurred: {e}"
+
+print(generate_response("Guten tag! How are you?"))
+>>>>>>> 3c64a5d7dd991056716409f1a8efe3cc9c005275
