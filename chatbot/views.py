7dfcb2a0f5a1ad49d47c67b03eba0d04b23d071f
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-09-10 17:31:36 +0200

    implementing huggingface and openai

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
+from .ai_model import generate_huggingface_response
+from .openai_model import generate_openai_response
 
-def index(request):
+def chatbot_view(request):
+    if request.method == "POST":
+        user_input = request.POST.get('user_input')
+        ai_choice = request.POST.get('ai_choice')
+
+        if ai_choice == 'huggingface':
+            response = generate_huggingface_response(user_input)
+        elif ai_choice == 'openai':
+            response = generate_openai_response(user_input)
+        else:
+            response = "Invalid AI choice."
+
+        return render(request, 'chatbot/index.html', {'response': response})
+
     return render(request, 'chatbot/index.html')
