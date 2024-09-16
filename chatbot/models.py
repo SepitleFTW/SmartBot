a9361b5288ffaa6d5ee4bf5896ac39dd9b3059a3
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-09-16 21:22:51 +0200

    fixing sqlite

--- chatbot/models.py
+++ chatbot/models.py
@@ -1,2 +1,18 @@
 from django.db import models
 
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
+
+class Convo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField()
+    response = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Conversation by {self.user.username} at {self.timestamp}"
--- chatbot/views.py
+++ chatbot/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .ai_model import generate_response
+from .models import User, Convo
 
+def save_convo(user_id, user_message, bot_response):
+    """
+    these functions will save and rettrieve conversations
+    """
+    user = User.objects.get(id=user.id)
+    Convo.objects.create(
+        user=user,
+        message=user_message,
+        response=bot_response
+    )
+
+
+def get_convo(user_id):
+    """this function as stated above will get the convos in history
+    """
+    user = User.objects.get(id=user_id)
+    return Convo.objects.filter(user=user).order_by('-timestamp')
 def index(request):
     return render(request, 'chatbot/index.html')
 
